dcc6444dd0920b2d1bfd2c1aa7cd5f144449b598
Sarah Kaydence <sarah.kydnc@gmail.com>
2022-04-25 14:01:46 +0800

    Removed redundant code

--- src/Photo.js
+++ src/Photo.js
@@ -29,23 +29,3 @@ function Photo(props) {
 }
 
 export default Photo;
-
-//return (
-//      <div className="search-photos">
-//        <div className="row">
-//          {props.photos.map(function (photo, index) {
-//            return (
-//              <div className="col-sm-3" key={index}>
-//                <a href={photo.src.original} target="_blank" rel="noreferrer">
-//                  <img
-//                    src={photo.src.tiny}
-//                    alt={photo.alt}
-//                    className="img-fluid"
-//                  />
-//                </a>
-//              </div>
-//            );
-//          })}
-//        </div>
-//      </div>
-//    );
